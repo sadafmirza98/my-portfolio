55557d854c9659da0b63f4d86473a4e5b897e488
sadafmirza98 <sadafmirza98@gmail.com>
2024-02-21 14:39:32 +0530

    Tooltip added

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -6,31 +6,45 @@ import {
   DiNodejs,
   DiMongodb,
   DiGit,
+  DiAngularSimple,
+  DiHtml5,
+  DiMysql
 } from "react-icons/di";
-import {
-  SiPostgresql,
-} from "react-icons/si";
 
 function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
+      <span className="tooltip">JavaScript</span>
         <DiJavascript1 />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <DiNodejs />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <span className="tooltip">React</span>
         <DiReact />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
+      <span className="tooltip">Angular</span>
+        <DiAngularSimple />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+      <span className="tooltip">HTML</span>
+        <DiHtml5 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+      <span className="tooltip">Node js</span>
+        <DiNodejs />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+      <span className="tooltip">MongoDB</span>
         <DiMongodb />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
+      <span className="tooltip">Git</span>
         <DiGit />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <SiPostgresql />
+      <span className="tooltip">SQL</span>
+        <DiMysql />
       </Col>
     </Row>
   );
--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -4,23 +4,42 @@ import {
   SiVisualstudiocode,
   SiPostman,
   SiDocker,
-  SiAmazonaws
+  SiAmazonaws,
+  SiJirasoftware,
+  SiPostgresql,
+  SiGithub
 } from "react-icons/si";
 
 function Toolstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
+        <span className="tooltip">Visual Studio Code</span>
         <SiVisualstudiocode />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
+      <span className="tooltip">Postman</span>
         <SiPostman />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
+      <span className="tooltip">Docker</span>
         <SiDocker />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
+      <span className="tooltip">AWS Console</span>
         <SiAmazonaws />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+      <span className="tooltip">Jira</span>
+        <SiJirasoftware />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+      <span className="tooltip">PostgreSQL</span>
+        <SiPostgresql />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+      <span className="tooltip">GitHub</span>
+        <SiGithub />
       </Col>
     </Row>
   );
