31aeb89b9629a14460f8d09673e72c2f5f44d042
sadafmirza98 <sadafmirza98@gmail.com>
2024-03-08 19:27:06 +0530

    modifications

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -19,22 +19,22 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={MapMemo}
-              isBlog={false}
-              title="Map Memo"
-              description="Map Memo is a website for organizing travel experiences. Users can save favorite locations, add memories, or create bucket lists for future visits. With authentication and authorization features, users can sign up, log in, and efficiently manage saved places. It's a valuable tool for adventurers to personalize their travel experiences."
-              ghLink="https://github.com/sadafmirza98/map-memo"
-              demoLink="In progress"
-            />
-          </Col>
-          <Col md={4} className="project-card">
-            <ProjectCard
               imgPath={GoalJutsu}
               isBlog={false}
               title="Goaljutsu"
               description="Goaljutsu is a Naruto-themed goal tracker crafted to empower users in managing, and achieving their goals through missions. With Goaljutsu, users can modify their goals while staying motivated with engaging Naruto interactions, fostering a fun and immersive goal-setting experience"
               ghLink="https://github.com/sadafmirza98/goaljutsu"
               demoLink="https://goaljutsu.netlify.app"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={MapMemo}
+              isBlog={false}
+              title="Map Memo"
+              description="Map Memo is a website for organizing travel experiences. Users can save favorite locations, add memories, or create bucket lists for future visits. With authentication and authorization features, users can sign up, log in, and efficiently manage saved places. It's a valuable tool for adventurers to personalize their travel experiences."
+              ghLink="https://github.com/sadafmirza98/map-memo"
+              demoLink="In progress"
             />
           </Col>
         </Row>
